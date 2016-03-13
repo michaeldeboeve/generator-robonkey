2466600a7ce749067d7b2c06c1293f9a9a2e9b48
Michaël de Boevé <michaeldeboeve@gmail.com>
2016-03-13 22:39:18 +0100

    added library options

--- app/index.js
+++ app/index.js
@@ -17,7 +17,14 @@ module.exports = generators.Base.extend({
           appversion: this.appversion,
           applicense: this.applicense,
           appauthor: this.appauthor,
-          appemail: this.appemail
+          appemail: this.appemail,
+          includeJQuery: this.includeJQuery,
+          includeWaypoints: this.includeWaypoints,
+          includeSignals: this.includeSignals,
+          includeD3: this.includeD3,
+          includeTweenmax: this.includeTweenmax,
+          includeEnquire: this.includeEnquire,
+          includeModernizr: this.includeModernizr
         };
 
     // Create Folders
@@ -32,11 +39,16 @@ module.exports = generators.Base.extend({
 
     // Copy Website Files
     this.fs.copy(sourceRoot + '/website/.htaccess', destRoot + '/website/.htaccess');
-    this.fs.copy(sourceRoot + '/website/index.html', destRoot + '/website/index.html');
+    this.fs.copyTpl(sourceRoot + '/website/index.html', destRoot + '/website/index.html', templateContext);
     // this.fs.copy(sourceRoot + '/website/browserconfig.xml', destRoot + '/website/browserconfig.xml');
     // this.fs.copy(sourceRoot + '/website/crossdomain.xml', destRoot + '/website/crossdomain.xml');
     // this.fs.copyTpl(sourceRoot + '/website/humans.txt', destRoot + '/website/humans.txt', templateContext);
     // this.fs.copy(sourceRoot + '/website/robots.txt', destRoot + '/website/robots.txt');
+
+    this.fs.copy(sourceRoot + '/project/.editorconfig', destRoot + '/.editorconfig');
+    this.fs.copy(sourceRoot + '/project/.gitignore', destRoot + '/.gitignore');
+    this.fs.copy(sourceRoot + '/project/.gitattributes', destRoot + '/.gitattributes');
+    this.fs.copy(sourceRoot + '/project/README.md', destRoot + '/README.md');
 
     // Copy Bower Files
     this.fs.copy(sourceRoot + '/bower/.bowerrc', destRoot + '/gulp/.bowerrc');
@@ -49,7 +61,14 @@ module.exports = generators.Base.extend({
     this.fs.copy(sourceRoot + '/gulp/config.json', destRoot + '/gulp/config.json');
 
     // Copy Src Files
-    this.fs.copy(sourceRoot + '/src', destRoot + '/src');
+    this.fs.copy(sourceRoot + '/src/iconfont', destRoot + '/src/iconfont');
+    this.fs.copy(sourceRoot + '/src/js', destRoot + '/src/js');
+    this.fs.copy(sourceRoot + '/src/scss', destRoot + '/src/scss');
+    this.fs.copy(sourceRoot + '/src/jade/index.jade', destRoot + '/src/jade/index.jade');
+    this.fs.copy(sourceRoot + '/src/jade/tools', destRoot + '/src/jade/tools');
+    this.fs.copy(sourceRoot + '/src/jade/templates', destRoot + '/src/jade/templates');
+    this.fs.copyTpl(sourceRoot + '/src/jade/templates/base.jade', destRoot + '/src/jade/templates/base.jade', templateContext);
+    this.fs.copy(sourceRoot + '/src/jade/templates/views', destRoot + '/src/jade/templates/views');
   },
 
   _copyBowerComponents: function() {
@@ -58,23 +77,38 @@ module.exports = generators.Base.extend({
     var srcsRoot = this.destinationRoot() + '/../src';
 
     // jQuery JS
-    this.fs.copy(bowerRoot + '/jquery/dist/jquery.min.js', destRoot + '/jquery.min.js');
+    if(this.includeJQuery) {
+      this.fs.copy(bowerRoot + '/jquery/dist/jquery.min.js', destRoot + '/jquery.min.js');
+    }
 
     // Waypoints JS
-    this.fs.copy(bowerRoot + '/waypoints/lib/jquery.waypoints.min.js', destRoot + '/waypoints/jquery.waypoints.min.js');
-    this.fs.copy(bowerRoot + '/waypoints/lib/noframework.waypoints.min.js', destRoot + '/waypoints/noframework.waypoints.min.js');
-    this.fs.copy(bowerRoot + '/waypoints/lib/waypoints.debug.js', destRoot + '/waypoints/waypoints.debug.js');
-    this.fs.copy(bowerRoot + '/waypoints/lib/waypoints.debug.js', destRoot + '/waypoints/waypoints.debug.js');
-    this.fs.copy(bowerRoot + '/waypoints/lib/shortcuts', destRoot + '/waypoints/shortcuts');
+    if(this.includeWaypoints) {
+      this.fs.copy(bowerRoot + '/waypoints/lib/jquery.waypoints.min.js', destRoot + '/waypoints/jquery.waypoints.min.js');
+      this.fs.copy(bowerRoot + '/waypoints/lib/noframework.waypoints.min.js', destRoot + '/waypoints/noframework.waypoints.min.js');
+      this.fs.copy(bowerRoot + '/waypoints/lib/waypoints.debug.js', destRoot + '/waypoints/waypoints.debug.js');
+      this.fs.copy(bowerRoot + '/waypoints/lib/waypoints.debug.js', destRoot + '/waypoints/waypoints.debug.js');
+      this.fs.copy(bowerRoot + '/waypoints/lib/shortcuts', destRoot + '/waypoints/shortcuts');
+    }
 
     // Enquire JS
-    this.fs.copy(bowerRoot + '/enquire/dist/enquire.min.js', destRoot + '/enquire.min.js');
+    if(this.includeEnquire) {
+      this.fs.copy(bowerRoot + '/enquire/dist/enquire.min.js', destRoot + '/enquire.min.js');
+    }
 
     // Signals JS
-    this.fs.copy(bowerRoot + '/js-signals/dist/signals.min.js', destRoot + '/signals.min.js');
+    if(this.includeSignals) {
+      this.fs.copy(bowerRoot + '/js-signals/dist/signals.min.js', destRoot + '/signals.min.js');
+    }
 
     // TweenMax JS
-    this.fs.copy(bowerRoot + '/gsap/src/minified', destRoot + '/gsap');
+    if(this.includeTweenmax) {
+      this.fs.copy(bowerRoot + '/gsap/src/minified', destRoot + '/gsap');
+    }
+
+    // D3 JS
+    if(this.includeD3) {
+      this.fs.copy(bowerRoot + '/d3/d3.min.js', destRoot + '/d3.min.js');
+    }
 
     // Reset SCSS
     this.fs.copy(bowerRoot + '/reset-css/_reset.scss', srcsRoot + '/scss/base/_reset.scss');
@@ -96,43 +130,123 @@ module.exports = generators.Base.extend({
   _getPrompts: function(){
     var prompts = [
       {
+        type: 'list',
+        name: 'setup',
+        message: 'What is your setup?',
+        choices: [{
+          name: 'basic',
+          value: 'isBasic',
+          checked: true
+        }, {
+          name: 'wordpress',
+          value: 'isWordpress',
+          checked: false
+        }, {
+          name: 'Drupal',
+          value: 'isDrupal',
+          checked: false
+        }, {
+          name: 'codeigniter',
+          value: 'isCodeigniter',
+          checked: false
+        }]
+      }
+      ,{
+        type: 'checkbox',
+        name: 'features',
+        message: 'What more would you like?',
+        choices: [{
+          name: 'Modernizr',
+          value: 'includeModernizr',
+          checked: true
+        }
+        ,{
+          name: 'jQuery',
+          value: 'includeJQuery',
+          checked: true
+        }
+        ,{
+          name: 'Waypoints',
+          value: 'includeWaypoints',
+          checked: false
+        }
+        ,{
+          name: 'Signals',
+          value: 'includeSignals',
+          checked: false
+        }
+        ,{
+          name: 'D3js',
+          value: 'includeD3',
+          checked: false
+        }
+        ,{
+          name: 'TweenMax',
+          value: 'includeTweenmax',
+          checked: false
+        }
+        ,{
+          name: 'Enquire',
+          value: 'includeEnquire',
+          checked: false
+        }]
+      }
+      ,{
         name: 'name',
-        message: 'What is the name of your project?',
+        message: 'Name your project:',
         default: this.appname
-      },
-      {
+      }
+      ,{
         name: 'description',
-        message: 'What is the description of your project?'
-      },
-      {
+        message: 'Describe your project:',
+        default: 'My new awesome app'
+      }
+      ,{
         name: 'version',
-        message: 'What is the version of your project?',
+        message: 'Project version:',
         default: '0.0.0'
-      },
-      {
+      }
+      ,{
         name: 'license',
-        message: 'How is your project licenced?',
+        message: 'Project licence:',
         default: 'MIT'
-      },
-      {
+      }
+      ,{
         name: 'yourname',
-        message: 'What is your name?'
-      },
-      {
+        message: 'Who the devil are you?',
+        default: 'Robonkey Team'
+      }
+      ,{
         name: 'email',
-        message: 'What is your email address?'
+        message: 'Your electronic mail address:',
+        default: 'robonkey@thegalaxy.com'
       }
     ];
     return prompts;
   },
 
   _saveAnswers: function(answers, callback) {
+    var features = answers.features;
+
+    function hasFeature(feat) {
+      return features && features.indexOf(feat) !== -1;
+    };
+
     this.appname = answers.name;
     this.appdescription = answers.description;
     this.appversion = answers.version;
     this.applicense = answers.license;
     this.appauthor = answers.yourname;
     this.appemail = answers.email;
+
+    this.includeJQuery = hasFeature('includeJQuery');
+    this.includeWaypoints = hasFeature('includeWaypoints');
+    this.includeSignals = hasFeature('includeSignals');
+    this.includeD3 = hasFeature('includeD3');
+    this.includeTweenmax = hasFeature('includeTweenmax');
+    this.includeEnquire = hasFeature('includeEnquire');
+    this.includeModernizr = hasFeature('includeModernizr');
+
     callback();
   },
 
@@ -157,7 +271,7 @@ module.exports = generators.Base.extend({
     var npmdir = process.cwd() + '/gulp';
     process.chdir(npmdir);
     this.bowerInstall();
-    this.npmInstall();
+    // this.npmInstall();
     this.on('end', function () {
       this._copyBowerComponents();
     });
