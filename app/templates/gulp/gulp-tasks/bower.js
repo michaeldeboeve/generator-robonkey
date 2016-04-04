0552dac629776a57cb172384616d19b5bb85dddb
Michaël de Boevé <michaeldeboeve@gmail.com>
2016-04-04 15:41:29 +0200

    fixed requirejs path

--- app/templates/gulp/gulp-tasks/bower.js
+++ app/templates/gulp/gulp-tasks/bower.js
@@ -3,6 +3,7 @@ var cfg             = JSON.parse(fs.readFileSync('./config.json'));
 var paths           = JSON.parse(fs.readFileSync('./paths.json'));
 var gulp            = require('gulp');
 var changed         = require('gulp-changed');
+var uglify          = require('gulp-uglify');
 
 var moveFiles = [];
 var bowerPath = '../src/bower_components/';
@@ -12,7 +13,7 @@ moveFiles.push(bowerPath + 'backbone/backbone-min.js');<% } %><% if(reactOption)
 moveFiles.push(bowerPath + 'react/react-dom.min.js', bowerPath + 'react/react-with-addons.min.js', bowerPath + 'react/react-dom-server.js', bowerPath + 'react/react.min.js');<% } %><% if(modernizrOption){ %>
 moveFiles.push('../src/modernizr/modernizr.dev.js');<% } %><% if(angularOption){ %>
 moveFiles.push(bowerPath + 'angular/angular.min.js');<% } %><% if(requireOption){ %>
-moveFiles.push(bowerPath + 'require/require.js');<% } %><% if(jqueryOption){ %>
+moveFiles.push(bowerPath + 'requirejs/require.js');<% } %><% if(jqueryOption){ %>
 moveFiles.push(bowerPath + 'jquery/dist/jquery.min.js');<% } %><% if(tweenmaxOption){ %>
 moveFiles.push(bowerPath + 'gsap/src/minified/TweenMax.min.js');<% } %><% if(d3jsOption){ %>
 moveFiles.push(bowerPath + 'd3/d3.min.js');<% } %><% if(enquireOption){ %>
@@ -28,6 +29,7 @@ if(moveFiles.length > 0) {
         dest = cfg.resrc.jsvendor;
 
     gulp.src(src)
+      // .pipe(uglify())
       .pipe(gulp.dest(dest));
   });
 } else {
